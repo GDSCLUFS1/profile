d2519e2865292e07752b22e748d1114808b43364
mashoun <mashoun.me@gmail.com>
2022-05-09 01:44:05 +0300

    v1

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            spinner:true,
             api: 'https://script.google.com/macros/s/AKfycbzAi8-666lI7ErYCoQXdksIuGDZXczn_J3wx0mxHBGoApCBfE5TzX9994DojwJ79WWL/exec',
             profile: ''
         }
@@ -10,7 +11,8 @@ createApp({
     mounted() {
         fetch(this.api).then(res => res.json()).then(res => {
             this.profile = res
-            console.log(this.profile)
+            // console.log(this.profile)
+            this.spinner = false;
         })
     }
 }).mount('#app')
